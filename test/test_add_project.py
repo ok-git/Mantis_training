3a32f552eef39fdfa8a7de43e2193dba25d91f71
Oleg <olka.contact@gmail.com>
2018-12-26 19:08:58 +0700

    Добавил генератор тестовых данных в фикстуру для test_add_project

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -2,10 +2,9 @@ from model.project import Project
 import pytest
 
 
-def test_add_project(app, db):
+def test_add_project(app, db, data_projects):
+    project = data_projects
     old_projects = db.get_project_list()
-    project = Project(name="Test Proj 6", status="development", inherit_global=True,
-                      view_status="public", description="Test_Description 6")
     if len(list(filter(lambda x: x.name == project.name, old_projects))) > 0:
         pytest.skip("duplicated project name detected - %s" % project.name)
     app.project.create(project)
