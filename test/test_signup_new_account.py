80bcda083ad18a405fb75cc988ad42fe4539bda9
Kalinin Oleg <okmail.inbox@gmail.com>
2019-01-06 23:50:43 +0700

    Для теста test_signup_new_account, реализована проверка через soap протокол

--- test/test_signup_new_account.py
+++ test/test_signup_new_account.py
@@ -1,12 +1,14 @@
 from data.projects import random_string as rnd_str
 
 
-def test_signup_new_account(app):
+def test_signup_new_account(app, check_ui):
     username = rnd_str("user_", 7, spaces=False)
     email = username + "@localhost"
     password = "test"
     app.james.ensure_user_exists(username, password)
     app.signup.new_user(username, email, password)
-    app.session.login(username, password)
-    assert app.session.is_logged_in_as(username)
-    app.session.logout()
+    assert app.soap.can_login(username, password)
+    if check_ui:
+        app.session.login(username, password)
+        assert app.session.is_logged_in_as(username)
+        app.session.logout()
